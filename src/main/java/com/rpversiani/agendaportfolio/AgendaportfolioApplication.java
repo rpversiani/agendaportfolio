0ee9eef4d22ac269ff59498ce08ad56b6a6b29b8
raphael_versiani <rpversiani@gmail.com>
2025-12-03 18:44:18 -0300

    Initial commit

---
+++ src/main/java/com/rpversiani/agendaportfolio/AgendaportfolioApplication.java
@@ -0,0 +1,13 @@
+package com.rpversiani.agendaportfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaportfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaportfolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rpversiani/agendaportfolio/AgendaportfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rpversiani.agendaportfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaportfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
